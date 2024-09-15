9f6b6390910395e99052b9d4cbe9e4e70a964205
rahapritam32 <rahapritam32@gmail.com>
2024-09-15 14:50:02 +0530

    order page done

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -12,10 +12,11 @@ export const GET = async (req: NextRequest) => {
   if (session) {
     try {
       if (session.user.isAdmin) {
-        const orders = prisma.order.findMany();
+        const orders = await prisma.order.findMany();
         return new NextResponse(JSON.stringify(orders), { status: 200 });
       }
-      const orders = prisma.order.findMany({
+      //if user is not admin, must be normal user
+      const orders = await prisma.order.findMany({
         where:{
             userEmail: session.user.email!
         }
