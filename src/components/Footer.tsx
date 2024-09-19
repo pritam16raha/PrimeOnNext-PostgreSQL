41d4c019c31c13288d9fde2447f088c2400d803f
rahapritam32 <rahapritam32@gmail.com>
2024-09-19 21:30:54 +0530

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link href="/" className="font-bold text-xl">MASSIMO</Link>
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-blue-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">Prime</Link>
       <p>© ALL RIGHTS RESERVED.</p>
     </div>
   );
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,18 +5,19 @@ import React, { useEffect, useState } from "react";
 const data = [
   {
     id: 1,
-    title: "always fresh & always crispy & always hot",
-    image: "/slide1.png",
+    title: "Rev Up Your Ride – Quality Parts & Accessories for Every Bike!",
+    image: "/photos/Banner/b5.jpg",
   },
   {
     id: 2,
-    title: "we deliver your order wherever you are in NY",
-    image: "/slide2.png",
+    title:
+      "Performance Unleashed – Premium Kits & Accessories for the Ultimate Ride.",
+    image: "/photos/Banner/b1.jpg",
   },
   {
     id: 3,
-    title: "the best pizza to share with your family",
-    image: "/slide3.jpg",
+    title: "Gear Up, Ride Strong – The One-Stop Shop for Bikers.",
+    image: "/photos/Banner/b2.jpg",
   },
 ];
 
@@ -35,11 +36,11 @@ const Slider = () => {
   return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50">
       {/* TEXT CONTAINER */}
-      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+      <div className="flex-1 flex items-center justify-center flex-col gap-8 text-gray-500 font-bold">
         <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
           {data[currentSlide].title}
         </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+        <button className="bg-blue-500 text-white py-4 px-8">Order Now</button>
       </div>
       {/* IMAGE CONTAINER */}
       <div className="w-full flex-1 relative">
